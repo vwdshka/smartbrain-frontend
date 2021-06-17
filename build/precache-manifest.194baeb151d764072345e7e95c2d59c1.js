0debccd438ba5da33e7bebfb95a12b05cbabf338
vwdshka <vwdshka@gmail.com>
2021-06-17 21:34:57 +0300

    commiting the react-built files

---
+++ build/precache-manifest.194baeb151d764072345e7e95c2d59c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407cfe13500a0ac9677bc58a4b48fd33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cc2d2283fa53ded19f2",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "86d6945cfa2f22311015",
+    "url": "/static/css/main.94cad480.chunk.css"
+  },
+  {
+    "revision": "7cc2d2283fa53ded19f2",
+    "url": "/static/js/2.66d37ec8.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.66d37ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d6945cfa2f22311015",
+    "url": "/static/js/main.fbdc4c8f.chunk.js"
+  },
+  {
+    "revision": "c9b0a537ac9221472b6e",
+    "url": "/static/js/runtime-main.30657a9b.js"
+  }
+]);
